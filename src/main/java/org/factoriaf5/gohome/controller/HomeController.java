8cd81a14d3ecb3cb28aa93f5707c49f4d4fe3c06
Desi y Lucas <desimorenoh@gmail.com>
2022-02-16 11:56:33 +0100

    organizando estructura de backend

---
+++ src/main/java/org/factoriaf5/gohome/controller/HomeController.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.gohome.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+    public class HomeController {
+        @GetMapping("/")
+        public String home() {
+            return "home";
+        }
+    }
--- src/test/java/org/factoriaf5/gohome/GoHomeApplicationTests.java
+++ src/test/java/org/factoriaf5/gohome/GoHomeApplicationTests.java
@@ -1,13 +1,27 @@
 package org.factoriaf5.gohome;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @SpringBootTest
-class GoHomeApplicationTests {
+@AutoConfigureMockMvc
+class ApplicationTests {
+
+    @Autowired
+    MockMvc mockMvc;
 
     @Test
-    void contextLoads() {
+    void loadsTheHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"));
     }
 
 }
