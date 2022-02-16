8746ebf7b7166e355b5ac7bb1ec365460056efa1
Desi y Lucas <desimorenoh@gmail.com>
2022-02-16 14:36:13 +0100

    test controlador home

--- src/test/java/org/factoriaf5/gohome/ApplicationTest.java
+++ src/test/java/org/factoriaf5/gohome/ApplicationTest.java
@@ -2,6 +2,7 @@ package org.factoriaf5.gohome;
 
 import org.factoriaf5.gohome.repositories.GoHome;
 import org.factoriaf5.gohome.repositories.GoHomeRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,11 +12,13 @@ import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
-
 @SpringBootTest
 @AutoConfigureMockMvc
 class ApplicationTests {
+    @BeforeEach
+    void setUp() {
+        goHomeRepository.deleteAll();
+    }
 
     @Autowired
     MockMvc mockMvc;
@@ -29,8 +32,9 @@ class ApplicationTests {
     @Autowired
     GoHomeRepository goHomeRepository;
 
+
     @Test
-    void returnsTheExistingBooks() throws Exception {
+    void returnsTheExistingHomes() throws Exception {
 
         GoHome goHome = goHomeRepository.save(new GoHome("Napoles", "http://2.bp.blogspot.com/-CPACB1sSmGs/Unvq3fKG4uI/AAAAAAAAHd8/iJoo2HB7dG4/s1600/fachada-de-casa-moderna-de-ladrillo-visto-de-2-pisos.jpg", "700", "670m2", "", "5"));
 
