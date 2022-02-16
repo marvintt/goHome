9294e473305a57d88fda908e46d1b8c22f11a053
Desi y Lucas <desimorenoh@gmail.com>
2022-02-16 10:51:42 +0100

    creación de proyecto

---
+++ src/main/java/org/factoriaf5/gohome/GoHomeApplication.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.gohome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/factoriaf5/gohome/GoHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.gohome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
